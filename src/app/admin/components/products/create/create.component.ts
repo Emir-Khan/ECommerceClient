df8f56036d3cadd07c9654da4ebeb1710dc05954
Emir-Khan <emirkhan5534@gmail.com>
2023-01-19 15:09:26 +0300

    Notification Update

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CreateProduct } from 'src/app/contracts/create-product';
+import { NotificationService, NotificationType } from 'src/app/services/admin/notification.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
@@ -9,19 +10,21 @@ import { ProductService } from 'src/app/services/common/models/product.service';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
 
   }
 
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
-    const createProduct:CreateProduct = new CreateProduct();
+    const createProduct: CreateProduct = new CreateProduct();
     createProduct.name = name.value
     createProduct.stock = parseInt(stock.value)
     createProduct.price = parseInt(price.value)
 
-    this.productService.create(createProduct);
+    this.productService.create(createProduct, () =>
+     this.notificationService.showNotification(NotificationType.Success, "Success", "Product Added")
+     , (msg: string) => this.notificationService.showNotification(NotificationType.Error, "Error", msg));
   }
 
 }
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CreateProduct } from 'src/app/contracts/create-product';
 import { HttpClientService } from '../http-client.service';
@@ -9,7 +10,20 @@ export class ProductService {
 
   constructor(private httpClientService: HttpClientService) { }
 
-  create(product: CreateProduct) {
-    this.httpClientService.post({ controller: "products" }, product).subscribe(result=>alert("basarili"))
+  create(product: CreateProduct, successCallBack?: any, errorCalllBack?: any) {
+    this.httpClientService.post({ controller: "products" }, product)
+      .subscribe({
+        next: result => successCallBack()
+        , error: (errorResult: HttpErrorResponse) => {
+          const _error: Array<{ key: string, value: Array<string> }> = errorResult.error;
+          let message = "";
+          _error.forEach((v, index) => {
+            v.value.forEach((_v, _index) => {
+              message += `${_v}<br>`;
+            });
+          })
+          errorCalllBack(message)
+        }
+      })
   }
 }
